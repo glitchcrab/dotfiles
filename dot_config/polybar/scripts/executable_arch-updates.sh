9e84c3216018c640a1bfa67776cac2b71f6af93b
Simon Weald <glitchcrab-github@simonweald.com>
2025-10-01 20:41:00 +0100

    polybar/arch updates: attempt to work around connectivity issues

--- dot_config/polybar/scripts/executable_arch-updates.sh
+++ dot_config/polybar/scripts/executable_arch-updates.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
-if ! updates_arch=$(checkupdates | wc -l); then
-    updates_arch=0
+updates_arch="0"
+updates_aur="0"
+
+if updates_arch=$(checkupdates 2>/dev/null | wc -l); then
+    :
+else
+    updates_arch="!"
 fi
 
-if ! updates_aur=$(paru -Qau | wc -l); then
-    updates_aur=0
+if updates_aur=$(paru -Qau 2>/dev/null | wc -l); then
+    :
+else
+    updates_aur="!"
 fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
-
-if [ "$updates" -gt 0 ]; then
-    #echo "$updates_arch  $updates_aur"
-    #echo "  ${updates_arch}/${updates_aur}"
+if [[ "$updates_arch" == "!" || "$updates_aur" == "!" ]]; then
     echo "${updates_arch}/${updates_aur}"
 else
-    echo ""
+    total=$((updates_arch + updates_aur))
+    if (( total > 0 )); then
+        echo "${updates_arch}/${updates_aur}"
+    else
+        echo ""
+    fi
 fi
